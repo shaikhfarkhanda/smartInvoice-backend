819499296941db9f69e2f98da3d2791b3ee9ae4f
shaikhfarkhanda <shaikhfarkhanda14@gmail.com>
2025-06-18 23:42:53 +0530

    Added user

--- invoices/views.py
+++ invoices/views.py
@@ -128,3 +128,9 @@ def send_invoice_email(request, pk):
 def login_status(request):
     return Response({"logged_in": True})
 
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
