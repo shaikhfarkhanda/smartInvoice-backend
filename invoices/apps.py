0968eec5c6008b82e081d544fb4023a8e5ab6621
shaikhfarkhanda <shaikhfarkhanda14@gmail.com>
2025-06-18 21:28:55 +0530

    Remove admin

--- invoices/apps.py
+++ invoices/apps.py
@@ -1,23 +1,5 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-import os
 
 class InvoicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'invoices'
-
-    def ready(self):
-        if os.environ.get("RUN_MAIN") != "true":
-            return
-
-        try:
-            User = get_user_model()
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="admin",
-                    email="admin@example.com",
-                    password="admin123"
-                )
-                print("✅ Superuser created!")
-        except Exception as e:
-            print("❌ Error creating superuser:", e)
